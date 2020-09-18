40b9020c5d01495d155b6bacaa9671aa0f893b08
luan goedert <luan_goedert2013@hotmail.com>
2020-09-18 16:07:04 -0300

    Other Entities

---
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,28 @@
+﻿using SalesWebMvc.Models.Enums;
+using System;
+
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+        public int ID { get; set; }
+        public DateTime Date { get; set; }
+        public double Ammount { get; set; }
+        public SaleStatus Status { get; set; }
+        public Seller Seller { get; set; }
+
+        public SalesRecord()
+        {
+        }
+
+        public SalesRecord(int iD, DateTime date, double ammount, SaleStatus status, Seller seller)
+        {
+            ID = iD;
+            Date = date;
+            Ammount = ammount;
+            Status = status;
+            Seller = seller;
+        }
+    }
+}
